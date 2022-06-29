42013ee154c64756b7b632bdcf9ed62ad1e58868
unknown <marcosga.correa@gmail.com>
2022-06-29 20:30:35 -0300

    start project chat

---
+++ chat/chat_flutter/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat_flutter/home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+void main() async {  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+    );
+  }
+}
